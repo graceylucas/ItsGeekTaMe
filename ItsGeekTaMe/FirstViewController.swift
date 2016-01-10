4d1d63e8775bd329e84a5e7b209b7731d388d68f
Mary Grace Lucas <graceylucas@hotmail.com>
2016-01-09 20:43:37 -0500

    Initial Commit

---
+++ ItsGeekTaMe/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ItsGeekTaMe
+//
+//  Created by Mary Grace Lucas on 1/9/16.
+//  Copyright © 2016 NinnyWorks. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ItsGeekTaMe/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ItsGeekTaMe
+//
+//  Created by Mary Grace Lucas on 1/9/16.
+//  Copyright © 2016 NinnyWorks. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
